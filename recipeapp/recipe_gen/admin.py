8084f563b0be6f326b2c20ab5b8e1041c413a080
albert-alvaro <albert.alvaro@my.jcu.edu.au>
2024-03-13 14:05:17 +0800

    registered the models, and updated some stuff

--- recipeapp/recipe_gen/admin.py
+++ recipeapp/recipe_gen/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Ingredients)
+admin.site.register(models.Prompts)
+admin.site.register(models.Recipe)
+admin.site.register(models.Recipe_History)
+admin.site.register(models.Users)
